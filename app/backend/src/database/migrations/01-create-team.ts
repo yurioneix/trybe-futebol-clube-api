dd59037727bbfbf772ce0a739e7db823fc15dc0c
yurioneix <yurioneix18@gmail.com>
2023-06-13 17:09:32 -0300

    Requisito 1: cria migration e model da tabela teams

--- app/backend/src/database/migrations/01-create-team.ts
+++ app/backend/src/database/migrations/01-create-team.ts
@@ -3,7 +3,7 @@ import ITeam from '../../Interfaces/ITeam';
 
 export default {
   up(queryInterface: QueryInterface) {
-    return queryInterface.createTable<Model<ITeam >>('trybe_eval', {
+    return queryInterface.createTable<Model<ITeam >>('teams', {
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -17,6 +17,6 @@ export default {
     });
   },
   down(queryInterface: QueryInterface) {
-    return queryInterface.dropTable('trybe_eval');
+    return queryInterface.dropTable('teams');
   },
 };
