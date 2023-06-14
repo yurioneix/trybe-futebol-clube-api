7f13a11a2373179c8bb4ad0ecafbaedc94e2b661
yurioneix <yurioneix18@gmail.com>
2023-06-14 18:59:16 -0300

    Corrige erro no JWT e nas validações

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -4,7 +4,10 @@ class Validations {
   static validateLogin(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
 
-    if (email === undefined || password === undefined) {
+    if (email === '' || email === undefined) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (password === '' || password === undefined) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
 
--- app/backend/src/services/TokenGeneratorJWT.ts
+++ app/backend/src/services/TokenGeneratorJWT.ts
@@ -1,11 +1,13 @@
 import * as jwt from 'jsonwebtoken';
 import IUser from '../Interfaces/IUser';
 
+const secret = process.env.JWT_SECRET;
+
 export default class TokenGeneratorJwt {
   private jwt = jwt;
 
   public generate(user: IUser): string {
-    const token = this.jwt.sign({ id: user.id }, 'SECRET');
+    const token = this.jwt.sign({ id: user.id }, secret as jwt.Secret);
     return token;
   }
 }
